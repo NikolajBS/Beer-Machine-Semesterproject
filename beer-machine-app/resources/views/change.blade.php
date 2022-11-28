20226e225814c2740e4d4042f86558247d286dcc
ZurenoC <92368939+ZurenoC@users.noreply.github.com>
2022-11-28 15:14:08 +0100

    new edit page

---
+++ beer-machine-app/resources/views/change.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{asset("css/main.css")}}">
+    <script defer src={{asset("javascript.js")}}></script>
+</head>
+<body>
+<header>
+    <img src="{{asset('images/banner.jpg')}}">
+</header>
+<div class="flex-container">
+
+    <main class="flex-container-main">
+        <label>Product Amount</label> <br>
+        <input></input> <br><br>
+
+        <label>Product Type</label> <br>
+        <select id="Beers"> Beers
+            <option value="0"> Pilsner</option>
+            <option value="1"> Wheat </option>
+            <option value="2"> IPA</option>
+            <option value="3"> Stout</option>
+            <option value="4"> Ale</option>
+            <option value="5"> Alcohol Free</option>
+        </select>
+    </main>
+</div>
+</body>
+</html>
