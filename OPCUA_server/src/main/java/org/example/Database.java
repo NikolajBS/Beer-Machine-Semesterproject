fb916931a3c64b315a404101ef888d10eb8b1189
impostermp <math-351@hotmail.com>
2022-12-02 01:10:46 +0100

    added logging method
    logging method creates a pdf with relevant information

--- OPCUA_server/src/main/java/org/example/Database.java
+++ OPCUA_server/src/main/java/org/example/Database.java
@@ -1,15 +1,50 @@
 package org.example;
 
 
+import java.awt.*;
+import java.io.FileOutputStream;
 import java.sql.*;
 import java.time.Instant;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
 
 public class Database {
 
     public void storeData(){
-
+        //
     }
+    public void readData(){
+        //
+    }
+    public static void logBatch(int id, String productType, int amount, double temperature, double humidity){
+        try{
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+            document.open();
 
+            Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+
+            Paragraph p1 = new Paragraph("Batch report ID#"+id,font);
+            p1.setAlignment(Element.ALIGN_CENTER);
+            document.add(p1);
+
+            document.add(new Paragraph("Product type: "+productType));
+            document.add(new Paragraph("Amount: "+amount));
+            document.add(new Paragraph("Average Temperature: "+temperature));
+            document.add(new Paragraph("Average Humidity: "+humidity));
+
+            //create new line in document
+            document.add(Chunk.NEWLINE);
+
+            document.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -25,7 +60,5 @@ public class Database {
         catch (Exception e){
             e.printStackTrace();
         }
-
-
     }
 }
