8f3b383e8512303db96d147d595000381adf3b4f
Nikolaj <nstau21@student.sdu.dk>
2022-12-04 14:33:04 +0100

    test example of sending json from java to laravel

--- OPCUA_server/src/main/java/org/example/TestJSON.java
+++ OPCUA_server/src/main/java/org/example/TestJSON.java
@@ -1,20 +1,21 @@
 package org.example;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class TestJSON {
-    private JSONArray getAPIData(){
+    private JSONObject getAPIData(){
         String GET_URL = "http://127.0.0.1:8000/test";
         URL obj = null;
         HttpURLConnection con = null;
         int responseCode = 0;
-        JSONArray json = null;
+        JSONObject json = null;
         try {
             obj = new URL(GET_URL);
             con = (HttpURLConnection) obj.openConnection();
@@ -37,7 +38,7 @@ public class TestJSON {
                     System.out.println(response);
                 }
                 reader.close();
-                json = new JSONArray(String.valueOf(response));
+                json = new JSONObject(String.valueOf(response));
             } else {
                 System.out.println("GET failed");
             }
@@ -46,8 +47,37 @@ public class TestJSON {
         }
         return json;
     }
+    private void sendData() {
+        String POST_URL = "http://127.0.0.1:8000/api/getdata";
+        String json ="{\"test\": 1}";
+        URL obj = null;
+        HttpURLConnection con = null;
+        int responseCode = 0;
+        try {
+            obj = new URL(POST_URL);
+            con = (HttpURLConnection) obj.openConnection();
+            con.setRequestProperty("Content-Type", "application/json;");
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            OutputStream output = con.getOutputStream();
+            output.write(json.getBytes());
+            output.close();
+            responseCode = con.getResponseCode();
+            System.out.println("POST Response Code :: " + responseCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            System.out.println("POST SUCCESS");
+        } else {
+            System.out.println("POST failed " + responseCode);
+        }
+    }
 
     public static void main(String[] args) {
-        new TestJSON().getAPIData();
+        //new TestJSON().getAPIData();
+        new TestJSON().sendData();
+
+        }
     }
-}
--- beer-machine-app/app/Http/Controllers/Controller.php
+++ beer-machine-app/app/Http/Controllers/Controller.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
@@ -16,6 +19,11 @@ class Controller extends BaseController
     public function changeEdit()
     {
         return view('change');
+    }
+
+    public function getApiData(Request $request){
+        $data = $request->test;
+        Log::info("Message is $data");
     }
 }
 
