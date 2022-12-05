30561b6c9611133a14e2073ae216cb6ca53768ea
Nikolaj <nstau21@student.sdu.dk>
2022-12-05 13:05:32 +0100

    Mistakes were made

---
+++ OPCUA_server/src/main/java/org/example/Database.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+import java.sql.*;
+import java.time.Instant;
+
+public class Database {
+
+    public void storeData(){
+
+    }
+
+
+    public static void main(String[] args) {
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/beers","root","secret");
+            PreparedStatement preState = connect.prepareStatement("INSERT INTO inventories (name, amount, created_at) VALUES(?,?,?)");
+            preState.setString(1, "Barley");
+            preState.setInt(2, 35000);
+            preState.setTimestamp(3, Timestamp.from(Instant.now()));
+            preState.execute();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
