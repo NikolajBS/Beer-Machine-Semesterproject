42e28814dab97e3193e6eb76a09d4be93d871afa
ZurenoC <92368939+ZurenoC@users.noreply.github.com>
2022-12-10 14:24:44 +0100

    updated write method

--- OPCUA_server/src/main/java/org/example/Write.java
+++ OPCUA_server/src/main/java/org/example/Write.java
@@ -27,44 +27,54 @@ import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 
 public class Write {
 
+    static OpcUaClient client;
+
     public static void main(String[] args) {
-        try 
-        {
+        try {
             List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
 
             OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
-            OpcUaClientConfig config;
-            for (int j = 0; j < endpoints.size(); j++) {
-                System.out.println("Endpoint "+ j + " is " + endpoints.get(j).getSecurityMode().name());
+            //cfg.setIdentityProvider(getIdentityProvider()).build();
 
-            }
             /*Selecting the endpoint connection with Security Mode/Security Policy == "None"*/
             for (int i = 0; i < endpoints.size(); i++) {
-                if(endpoints.get(i).getSecurityMode().name().equals("SignAndEncrypt")){ //None or SignAndEncrypt
+                if (endpoints.get(i).getSecurityMode().name().equals("None")) {
                     EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(i), "127.0.0.1", 4840);
                     cfg.setEndpoint(configPoint);
                     break;
                 }
             }
 
-            config = cfg.setIdentityProvider(getIdentityProvider()).build();
-            OpcUaClient client = OpcUaClient.create(config);
+            client = OpcUaClient.create(cfg.build());
             client.connect().get();
-
-            /* myVariable endpoint */
-            //NodeId nodeId  = NodeId.parse("ns=2;i=1002);
-            NodeId nodeId  = new NodeId(6, "::Program:Cube.Command.MachSpeed");
-            System.out.println(client.writeValue(nodeId, DataValue.valueOnly(new Variant((float)5.0))).get());
-
-        }
-        catch(Throwable ex)
-        {
+        } catch (Throwable ex) {
+            //System.out.println("*****************************************");
             ex.printStackTrace();
         }
 
+        //datatype hvis float skal castes som (float)value
+        //datatype hvis det er enten en boolean eller en int skal den IKKE CASTES!
+
+        //Speed
+        NodeId nodeSpeed  = new NodeId(6, "::Program:Cube.Command.MachSpeed");
+        // client.writeValue(nodeSpeed, DataValue.valueOnly(new Variant((float)600.0)));
+
+        //Parameters
+        NodeId nodePara0 = new NodeId(6, "::Program:Cube.Command.Parameter[0].Value");
+        NodeId nodePara1  = new NodeId(6, "::Program:Cube.Command.Parameter[1].Value");
+        NodeId nodePara2  = new NodeId(6, "::Program:Cube.Command.Parameter[2].Value");
+        // client.writeValue(nodePara0, DataValue.valueOnly(new Variant((float)10.0)));
+        //  client.writeValue(nodePara2, DataValue.valueOnly(new Variant((float)200.0)));
+
+
+        //Control
+        NodeId nodeControl  = new NodeId(6, "::Program:Cube.Command.CntrlCmd");
+        //client.writeValue(nodeControl, DataValue.valueOnly(new Variant(2)));
+        NodeId nodeSend  = new NodeId(6, "::Program:Cube.Command.CmdChangeRequest");
+        //client.writeValue(nodeSend, DataValue.valueOnly(new Variant(true)));
+
+
     }
-    public static IdentityProvider getIdentityProvider() {
-        return new UsernameProvider("sdu", "1234");
-    }
+
 
 }
